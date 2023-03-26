f8cec64d9496ecd0ba311c79ddbf27a0eb969f57
mustafiz <mustafizansari35@gmail.com>
2023-03-26 09:49:41 +0530

    Trial

---
+++ src/components/UpdateTrans.js
@@ -0,0 +1,22 @@
+
+
+import React from 'react'
+
+function UpdateTrans() {
+    return (
+        <div>
+            <div class="card h-100">
+                <div class="card-header">
+                    Featured
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Special title treatment</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default UpdateTrans
